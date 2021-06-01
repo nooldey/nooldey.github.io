5ab9abce6002cf9ae43e105ae748dd9ccfcbb5e0
nooldey-automator <zws@nooldey.local>
2021-06-01 14:43:41 +0000

    published

---
+++ gist/npm/dependency.sh
@@ -0,0 +1,12 @@
+# /*
+#  * @Author: zws <nooldey@gmail.com> 
+#  * @Date: 2021-05-07 15:24:12 
+#  * @Last Modified by: zws 
+#  * @Last Modified time: 2021-05-07 15:24:12 
+#  * @Description:  npm install 报错 Cloud not resolve dependency
+#  */
+
+ `npm i --legacy-peer-deps`
+
+#  完事。
+
